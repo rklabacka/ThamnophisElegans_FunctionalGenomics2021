f79c3e3d53de6015611e0c6ca52f8bb5ac4e9c93
Randy L. Klabacka <klabacka.randy@gmail.com>
2021-02-09 15:35:02 -0600

    shrinkGFF.py updated with shrinkGFF_v5.py

--- pythonScripts/shrinkGFF.py
+++ pythonScripts/shrinkGFF.py
@@ -1,24 +1,49 @@
 #! usr/bin/env/ python
 import sys
+import re
 import copy
 from collections import OrderedDict
+from collections import Counter
 
-##### CLASS LOCUS #####
-class Locus(object):
-    def __init__(self, exon_in, locus_name_in, start_in, stop_in):
+
+##### CLASS EXON #####
+class Exon(object):
+    def __init__(self, exon_in, contig_name_in, start_in, stop_in):
         self.exon = exon_in
-        self.locus_name = locus_name_in
+        self.gene = exon_in.split("_")[0]
+        self.contig_name = contig_name_in
         self.start = start_in
         self.stop = stop_in
 ### Getters
     def getExon(self):
         return self.exon
-    def getName(self):
-        return self.locus_name
+    def getContig(self):
+        return self.contig_name
     def getStart(self):
         return self.start
     def getStop(self):
         return self.stop
+    def getGene(self):
+        return self.gene
+
+##### CLASS GENE #####
+class Gene(object):
+    def __init__(self, gene_in, first_candLocus):
+        self.gene = gene_in
+        self.candidateLoci = [first_candLocus]
+### Getters
+    def getGene(self):
+        return self.gene
+    def getCandLoci(self):
+        return self.candidateLoci
+### Setters
+    def addCandLocus(self, locus_in):
+        self.candidateLoci.append(locus_in)
+
+def most_frequent(List):
+    occurence_count = Counter(List)
+    return occurence_count.most_common(1)[0][0]
+
 
 #Open infile with all genes (blast output from BlastResults.txt). Infile nature is very important:
 #   Column 0: Locus name FROM INFERRED EXONS
@@ -36,21 +61,24 @@ GFF_exons_out = open(sys.argv[4], 'w')
 GFF_cds_out = open(sys.argv[5], 'w')
 log = open(sys.argv[6], 'w')
 
-locus_dict = {}
-num = 0
+exon_dict = {}
+gene_dict = {}
+capturedGenes = []
+gffGenes = []
+num = 0 
 
-for line in BlastResults:
-    line = line.split("\t")
-    exon = line[0]
-    exon = exon + "_" + str(num)
-    locus = line[1]
-    start = line[5]
-    stop = line[6].strip()
-    new_locus = Locus(exon, locus, start, stop)
-    locus_dict[exon] = new_locus
-    num += 1
+for blast in BlastResults:
+    blast = blast.split("\t")
+    exon = blast[0]
+    gene = exon.split("_")[0]
+    log.write("Gene: " + gene + '\n')
+    if exon not in exon_dict:
+        contig = blast[1]
+        start = blast[5]
+        stop = blast[6].strip()
+        new_exon = Exon(exon, contig, start, stop)
+        exon_dict[exon] = new_exon
 
-targetGene = False
 gff_dict = {}
 for line_unsplit in GFF_read:
     line = line_unsplit.split("\t")
@@ -59,22 +87,60 @@ for line_unsplit in GFF_read:
     haystack_start = line[3]
     haystack_stop = line[4]
     if kind == "gene":
-        targetGene = False
-        for needle in locus_dict:
-            if locus_dict[needle].getName() == haystack_locus:
-                if ((int(haystack_start) <= int(locus_dict[needle].getStart())) and (int(haystack_stop) >= int(locus_dict[needle].getStop()))):
-                    targetGene = True
-                    GFF_genes_out.write(line_unsplit) 
-                    log.write("Exon <" + locus_dict[needle].getExon() + " start: " + str(locus_dict[needle].getStart()) + ", stop: " + str(locus_dict[needle].getStop()) + "> found in GFF as: <" + line_unsplit.strip() + " ; with start: " + str(haystack_start) + " and stop: " + str(haystack_stop) + ">\n")
-    elif ((kind == "CDS") and (targetGene == True)):
-        GFF_cds_out.write(line_unsplit)
-    elif ((kind == "exon") and (targetGene == True)):
-        GFF_exons_out.write(line_unsplit)
+        for needle in exon_dict:
+            if exon_dict[needle].getContig() == haystack_locus:
+                if ((int(haystack_start) <= int(exon_dict[needle].getStart())) and (int(haystack_stop) >= int(exon_dict[needle].getStop()))):
+#                    GFF_genes_out.write(line_unsplit) 
+                    candLocus = line[8].split("gene=")[1]
+                    candLocus = candLocus.split(";")[0]
+                    needleGene = exon_dict[needle].getGene()
+                    if needleGene in gene_dict:
+                        # add candLocus to gene object
+                        gene_dict[needleGene].addCandLocus(candLocus)
+                    else:
+                        # create gene object and initialize with candLocus
+                        new_gene = Gene(needleGene, candLocus)
+                        gene_dict[needleGene] = new_gene
+                    log.write("Exon " + exon_dict[needle].getExon() + "found in GFF as: " + candLocus + "\n")
+
+GFF_read.close()
+log.write("\n\n\nFIND CAPTURED GENES:")
+for gene in gene_dict:
+    capturedGene = most_frequent(gene_dict[gene].getCandLoci())
+    log.write(gene + ": " + capturedGene + "\n")
+    capturedGenes.append(capturedGene)
           
+log.write("\n\nFinding the captured genes in the GFF\n")
+
+GFF_read = open(sys.argv[2], 'r')
+for region in GFF_read:
+    region_split = region.split("\t")
+    kind = region_split[2]
+    if "gene=" in region_split[8]:
+        gene_inquire = region_split[8].split("gene=")[1]
+        gene_inquire = gene_inquire.split(";")[0]
+        log.write("GFF gene: " + gene_inquire + "\n")
+        if gene_inquire in capturedGenes:
+            if kind == "gene":
+                GFF_genes_out.write(region) 
+                gffGenes.append(gene_inquire)
+            elif kind == "exon":
+                GFF_exons_out.write(region)
+            elif kind == "CDS":
+                GFF_cds_out.write(region)
+        else:
+            log.write(gene_inquire + " has no match\n")
+
+log.write("\n\nCaptured gene list:\n")
+for i in capturedGenes:
+    log.write(i + "\n")
+
+log.write("\n\nDuplicates in captured gene list:\n")
+log.write([item for item, count in Counter(capturedGenes).items() if count > 1])
 
 BlastResults.close()
 GFF_read.close()
 GFF_genes_out.close()
-GFF_exons_out.close
-GFF_cds_out.close
+GFF_exons_out.close()
+GFF_cds_out.close()
 log.close()
