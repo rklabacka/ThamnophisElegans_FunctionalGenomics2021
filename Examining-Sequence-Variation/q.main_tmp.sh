f2028e2e2d3e34026f6133f143996afc85e25476
rlk0015 <rlk0015@c20-login01.cm.cluster>
2022-02-04 15:55:09 -0600

    modified files for compatibility with slurm on Easley

--- Examining-Sequence-Variation/q.main_tmp.sh
+++ Examining-Sequence-Variation/q.main_tmp.sh
@@ -1,42 +1,13 @@
 #!/bin/sh
-
-# This is the bash script used to process raw reads for the garter snake 
-# targeted gene network study (for gene expression and populationi genetics)
-
-# Prepared by Randy Klabacka
-
-# -- Job Details for Hopper cluster at Auburn University-- #
-#Give job a name
-#PBS -N FullScript_May2020
-
-#-- We recommend passing your environment variables down to the
-#-- compute nodes with -V, but this is optional
-#PBS -V
-
-#-- Specify the number of nodes and cores you want to use
-#-- Hopper's standard compute nodes have a total of 20 cores each
-#-- so, to use all the processors on a single machine, set your
-#-- ppn (processors per node) to 20.
-#PBS -l nodes=1:ppn=1,walltime=05:00:00:00
-#-- Indicate if\when you want to receive email about your job
-#-- The directive below sends email if the job is (a) aborted, 
-#-- when it (b) begins, and when it (e) ends
-#PBS -m abe rlk0015@auburn.edu
-
-echo ------------------------------------------------------
-echo -n 'Job is running on node '; cat $PBS_NODEFILE
-echo ------------------------------------------------------
-echo PBS: qsub is running on $PBS_O_HOST
-echo PBS: originating queue is $PBS_O_QUEUE
-echo PBS: executing queue is $PBS_QUEUE
-echo PBS: working directory is $PBS_O_WORKDIR
-echo PBS: execution mode is $PBS_ENVIRONMENT
-echo PBS: job identifier is $PBS_JOBID
-echo PBS: job name is $PBS_JOBNAME
-echo PBS: node file is $PBS_NODEFILE
-echo PBS: current home directory is $PBS_O_HOME
-echo PBS: PATH = $PBS_O_PATH
-echo ------------------------------------------------------
+#SBATCH --job-name=Thamnophis_main_tmp
+#SBATCH --nodes=1                     # node(s) required for job
+#SBATCH --ntasks=1                    # number of tasks across all nodes
+#SBATCH --partition=general           # name of partition
+#SBATCH --time=20:00:00               # Run time (D-HH:MM:SS)
+#SBATCH --output=test-%j.out          # Output file. %j is replaced with job ID
+#SBATCH --error=test_error-%j.err     # Error file. %j is replaced with job ID
+#SBATCH --mail-type=ALL               # will send email for begin,end,fail
+#SBATCH --mail-user=rlk0015@auburn.edu
 
 WorkingDirectory=/scratch/rlk0015/Telegans_SeqCapProject/WorkingDirectory
 pythonScripts=/home/rlk0015/projects/Thamnophis/ThamnophisElegans_FunctionalGenomics2021/Examining-Sequence-Variation/pythonScripts
@@ -76,13 +47,13 @@ function loadModules {
 # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 # The following functions are called from the reads2vcf.sh script
 # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-cd /home/rlk0015/SeqCap/code/GenomicProcessingPipeline/Examining-Sequence-Variation
-source reads2vcf.sh
+cd /home/rlk0015/projects/Thamnophis/ThamnophisElegans_FunctionalGenomics2021/Examining-Sequence-Variation
+# source reads2vcf.sh
 
  # prepare environment
  # for Hopper: loadModules_Hopper
  loadModules # If you encounter errors, see if modules from loadModules_Hopper need inclusion
- createWorkingEnvironment-reads2vcf
+ # createWorkingEnvironment-reads2vcf
  
 #+ COMPLETE  ## ------------------------
 #+ COMPLETE  # begin raw read processing
@@ -197,8 +168,8 @@ source reads2vcf.sh
 #+ COMPLETE  # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 #+ COMPLETE  # The following functions are called from the SNP_curation.sh script
 #+ COMPLETE  # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-#+ COMPLETE  cd /home/rlk0015/SeqCap/code/GenomicProcessingPipeline/Examining-Sequence-Variation
-#+ COMPLETE  source SNP_curation.sh
+cd /home/rlk0015/projects/Thamnophis/ThamnophisElegans_FunctionalGenomics2021/Examining-Sequence-Variation
+source ./SNP_curation.sh
 #+ COMPLETE  
 #+ COMPLETE    cd $WorkingDirectory/variantFiltration
 #+ COMPLETE    ## -- create full dataset (combining Jessica's WGS data with Seq-Cap+RNA-Seq)
--- Examining-Sequence-Variation/reads2vcf.sh
+++ Examining-Sequence-Variation/reads2vcf.sh
@@ -1,43 +1,5 @@
 #!/bin/sh
 
-# This is the bash script used to process raw reads for the garter snake 
-# targeted gene network study (for gene expression and populationi genetics)
-
-# Prepared by Randy Klabacka
-
-# -- Job Details for Hopper cluster at Auburn University-- #
-#Give job a name
-#PBS -N FullScript_May2020
-
-#-- We recommend passing your environment variables down to the
-#-- compute nodes with -V, but this is optional
-#PBS -V
-
-#-- Specify the number of nodes and cores you want to use
-#-- Hopper's standard compute nodes have a total of 20 cores each
-#-- so, to use all the processors on a single machine, set your
-#-- ppn (processors per node) to 20.
-#PBS -l nodes=1:ppn=10,walltime=05:00:00:00
-#-- Indicate if\when you want to receive email about your job
-#-- The directive below sends email if the job is (a) aborted, 
-#-- when it (b) begins, and when it (e) ends
-#PBS -m abe rlk0015@auburn.edu
-
-echo ------------------------------------------------------
-echo -n 'Job is running on node '; cat $PBS_NODEFILE
-echo ------------------------------------------------------
-echo PBS: qsub is running on $PBS_O_HOST
-echo PBS: originating queue is $PBS_O_QUEUE
-echo PBS: executing queue is $PBS_QUEUE
-echo PBS: working directory is $PBS_O_WORKDIR
-echo PBS: execution mode is $PBS_ENVIRONMENT
-echo PBS: job identifier is $PBS_JOBID
-echo PBS: job name is $PBS_JOBNAME
-echo PBS: node file is $PBS_NODEFILE
-echo PBS: current home directory is $PBS_O_HOME
-echo PBS: PATH = $PBS_O_PATH
-echo ------------------------------------------------------
-
 # -- Functions -- #
 ## --- To see implementation of functions see q.main.sh--- ###
 
