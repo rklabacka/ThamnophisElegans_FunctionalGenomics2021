637df7a13d2931f7692b8526416b090bb0aacaea
rlk0015 <rlk0015@c20-login01.cm.cluster>
2022-02-04 14:50:06 -0600

    modified getTranscriptLengths.py to only include the longest CDS (up to this point it had been concatenating lengths, thus incorrectly inflating the calculated coding sequence size)

--- Examining-Sequence-Variation/pythonScripts/getTranscriptLengths.py
+++ Examining-Sequence-Variation/pythonScripts/getTranscriptLengths.py
@@ -9,17 +9,20 @@ Arguments:
               number of variant sites
               (column 2)
     argv[3] : title of output file
+    argv[4] : log file
 """
 
 import sys
 import re
 
+
 class Gene(object):
-    def __init__(self, name_in, leng_in):
+    def __init__(self, name_in):
         self.name = name_in
-        self.leng = leng_in
+        self.leng_list = []
         self.nvar = 0
         self.pervar = 0
+        self.longestCDS = 0
     def getName(self):
         return self.name
     def getLeng(self):
@@ -28,25 +31,42 @@ class Gene(object):
         return self.nvar
     def getPervar(self):
         return self.pervar
+    def getLongestCDS(self):
+        return self.longestCDS
+    # setters
     def setNvar(self, nvar_in):
         self.nvar = nvar_in.strip()
-        self.pervar = float(self.nvar) / float(self.leng)
+        if self.longestCDS != 0:
+            self.pervar = float(self.nvar) / float(self.longestCDS)
+    def addLeng(self, leng_in):
+        self.leng_list.append(leng_in)
+    def setLongestCDS(self):
+        self.longestCDS = max(self.leng_list)
 
 def get_gene_dict(gff_in):
     gene_dict = {}
     leng = 0
     past_gene = "start"
-    
+    past_cds = "start"    
     for line in GFF_read:
         line = line.split("\t")
         curr_gene = line[8]
         curr_gene = curr_gene.split("gene=")[1]
         curr_gene = curr_gene.split(";")[0]
-        if curr_gene != past_gene:
-            gene_obj = Gene(past_gene, leng)
-            gene_dict[past_gene] = gene_obj
+        curr_cds = line[8].split("ID=cds-")[1]
+        curr_cds = curr_cds.split(";")[0]
+        if curr_cds != past_cds:
+            if past_gene in gene_dict:
+                gene_dict[past_gene].addLeng(leng)
             leng = 0
-            past_gene = curr_gene
+            past_cds = curr_cds
+            if curr_gene != past_gene:
+                if past_gene in gene_dict:
+                    gene_dict[past_gene].setLongestCDS()
+                    log.write("\nGene: " + gene_dict[past_gene].getName() + ", longest CDS: " + str(gene_dict[past_gene].getLongestCDS()))
+                gene_obj = Gene(curr_gene)
+                gene_dict[curr_gene] = gene_obj
+                past_gene = curr_gene
         leng = leng + (int(line[4]) - int(line[3]))    
     return gene_dict
 
@@ -62,10 +82,11 @@ def get_variant_lengths(gene_dict, variants_read, transcript_lengths):
         for needle in gene_dict:
             if gene_dict[needle].getName().strip() == haystack_gene.strip():
                 gene_dict[needle].setNvar(nvar)
-                transcript_lengths.write(gene_dict[needle].getName() + "\t" + str(gene_dict[needle].getNvar()) + "\t" + str(gene_dict[needle].getLeng()) + "\t" + str(gene_dict[needle].getPervar()) + "\n")
+                transcript_lengths.write(gene_dict[needle].getName() + "\t" + str(gene_dict[needle].getNvar()) + "\t" + str(gene_dict[needle].getLongestCDS()) + "\t" + str(gene_dict[needle].getPervar()) + "\n")
 
 if __name__ == "__main__":
 
+    log = open(sys.argv[4], 'w')
     gene_dict = {}
 
     with open(sys.argv[1], 'r') as GFF_read:
@@ -73,5 +94,5 @@ if __name__ == "__main__":
         with open(sys.argv[2], 'r') as variants_read:
             with open(sys.argv[3], 'w') as transcript_lengths:
                 get_variant_lengths(gene_dict, variants_read, transcript_lengths)
-
+    log.close()
 
